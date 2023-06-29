90db23c93cf028e0a2c5dd1d553ff08e9ce58fee
Felipe Soares <felipe.11.11@hotmail.com>
2023-06-29 12:09:19 -0400

    responsiveness

--- src/Components/Profile/index.jsx
+++ src/Components/Profile/index.jsx
@@ -6,13 +6,13 @@ export default function Profile({avatar, name, bio, email, phone, githubUrl, lin
         <>
             <img src={avatar} alt={name}/>
             <h2 className="name borderBottom">{name}</h2>
-            
+            <hr />
             <p className="bio borderBottom">{bio}</p>
-            
+            <hr />
             <p className="phone borderBottom">{phone}</p>
-            
+            <hr />
             <a href={"mailto:"+ email} className="email borderBottom">{email}</a>
-            
+            <hr />
             <Button text="GitHub" url={githubUrl}/>
             <Button text="Linkedin" url={linkedinUrl}/>
             <Button text="Curso" url={courseUrl}/>
